e2324d8d931e7c81b7c30195e1afd9781fed5e40
Dhairya <dhairyagulati370@gmail.com>
2023-02-04 16:13:29 +0530

    update : email card CSS

--- src/components/emailCard/EmailCard.jsx
+++ src/components/emailCard/EmailCard.jsx
@@ -4,17 +4,18 @@ function EmailCard(emailData) {
   const emailInfo = emailData.emailData;
   return (
     <div className="email-card">
-      EmailCard
       <div className="email-profile">
         {emailInfo.from.email.charAt(0).toUpperCase()}
       </div>
-      <div className="email-from">
-        {emailInfo.from.name}
-        {emailInfo.from.email}
+      <div className="email-info">
+        <div className="email-from">
+          From : {emailInfo.from.name}
+          {emailInfo.from.email}
+        </div>
+        <div className="email-subject">Subject : {emailInfo.subject}</div>
+        <div className="email-description">{emailInfo.short_description}</div>
+        <div className="email-time">{emailInfo.date}.</div>
       </div>
-      <div className="email-subject">subject : {emailInfo.subject}</div>
-      <div className="email-description">{emailInfo.short_description}</div>
-      <div className="email-time">{emailInfo.date}.</div>
     </div>
   );
 }
