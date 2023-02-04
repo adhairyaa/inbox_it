d7d1c486c36ee7780a7bef89ff2bd39bd89aa1fa
Dhairya <dhairyagulati370@gmail.com>
2023-02-04 12:25:51 +0530

    Add : Redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import EmailListing from "./pages/emailListing/EmailListing";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <EmailListing />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/emailListing/EmailListing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EmailListing() {
+  return (
+    <div className="email-listing-page">
+      <div className="email-cards"></div>
+    </div>
+  );
+}
+
+export default EmailListing;
