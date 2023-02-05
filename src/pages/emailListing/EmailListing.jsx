192c8f37309d67aec5b3e463de5d8acb0cabc2a5
Dhairya <dhairyagulati370@gmail.com>
2023-02-05 15:58:09 +0530

    Add : filter component

--- src/pages/emailListing/EmailListing.jsx
+++ src/pages/emailListing/EmailListing.jsx
@@ -3,6 +3,7 @@ import "./EmailListing.css";
 import { useSelector, useDispatch } from "react-redux";
 import EmailCard from "../../components/emailCard/EmailCard";
 import { getEmails } from "../../features/emails/emailSlice";
+import Filter from "../../components/emailCard/filter/Filter";
 function EmailListing() {
   const { emails, status } = useSelector((state) => state.emails);
   const dispatch = useDispatch();
@@ -12,6 +13,7 @@ function EmailListing() {
 
   return (
     <div className="email-listing-page">
+      <Filter />
       <div className="email-cards">
         {status === "fulfilled" ? (
           emails.map((email) => <EmailCard emailData={email} />)
