056f2c7ed2b403adeb6f7c2cacc707d0f57757c9
Dhairya <dhairyagulati370@gmail.com>
2023-02-06 16:54:25 +0530

    add : Filter Slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import activeEmailReducer from "../features/activeEmail/activeEmailSlice";
 import emailReducer from "../features/emails/emailSlice";
+import filterReducer from "../features/filter/filterSlice";
 
 export const store = configureStore({
   reducer: {
     emails: emailReducer,
     activeEmail: activeEmailReducer,
+    filters: filterReducer,
   },
 });
